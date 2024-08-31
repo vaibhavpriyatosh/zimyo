e7b1c6a837dd7eb0e594e68a974e8a6cf076e079
vaibhavpriyatosh <priyatoshvaibhav5@gmail.com>
2024-08-31 16:55:05 +0530

    first commit

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+	res.send('Hello, Express with TypeScript and dotenv!');
+});
+
+app.listen(port, () => {
+	console.log(`Server is running on http://localhost:${port}`);
+});
